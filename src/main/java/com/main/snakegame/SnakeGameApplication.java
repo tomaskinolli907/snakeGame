7e4095e2317886856b517dcb9cc3711de7162477
tomas <tomaskinolli909@icloud.com>
2025-02-05 22:16:08 -0500

    initial upload

--- src/main/java/com/main/snakegame/SnakeGameApplication.java
+++ src/main/java/com/main/snakegame/SnakeGameApplication.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class SnakeGameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        //snake game
         FXMLLoader fxmlLoader = new FXMLLoader(SnakeGameApplication.class.getResource("View/Menu-Screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500 , 500);
         stage.setResizable(false);
